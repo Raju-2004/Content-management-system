05d0c0ce3d37f6bd927e40579a41a644ddcc08f1
kusa raju <21pa1a05e9@vishnu.edu.in>
2024-02-05 18:54:22 +0530

    mongodb changed to atlas

--- server/config/configuraion.js
+++ server/config/configuraion.js
@@ -1,6 +1,6 @@
 module.exports ={
-    mongoDbUrL : "mongodb://127.0.0.1/ContentCraft",
-    PORT : process.env.PORT || 5000,
+    mongoDbUrL : "mongodb+srv://kusaraju:HGJgDfMjzywf43dN@cluster0.yjvca42.mongodb.net/ContentCraft",
+    PORT : process.env.PORT || 4000,
     globalVariables :(req,res,next)=>{
         res.locals.success_message = req.flash('success-message');
         res.locals.error_message = req.flash('error-message');
